14c918e82f9ca54dc9387241c2cef3115d307e51
Roei Schuster <rs864@cornell.edu>
2019-03-11 18:19:58 -0400

    added timing usable by libtorch_python.so

---
+++ timing/roeistimer.c
@@ -0,0 +1,11 @@
+//#include "roeistimer.h"
+#include <stdio.h>
+#include <stdint.h>
+
+
+#define MAX_FUNCS 500
+const char* roeis_func_names[MAX_FUNCS];
+uint64_t roeis_times[MAX_FUNCS];
+uint32_t roeis_funcs_len = 0;
+
+
