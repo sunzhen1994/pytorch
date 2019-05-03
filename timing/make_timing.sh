daf7e8f9ea30dcb9ef745c3464678098ed63cb96
Roei Schuster <rs864@cornell.edu>
2019-05-03 14:45:41 -0400

    changes

---
+++ timing/make_timing.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+gcc -c -Wall -Werror -fpic roeistimer.c &&
+gcc -shared -o libroeistimer.so roeistimer.o &&
+cp libroeistimer.so /home/roei/.local/lib/python2.7/site-packages/torch/lib/
+#cp libroeistimer.so /usr/local/lib/python2.7/dist-packages/torch/lib/
+cp libroeistimer.so ../build/lib/
+cp libroeistimer.so ../build/lib.linux-x86_64-2.7/torch/lib/
+cp libroeistimer.so ../torch/lib/
+cp libroeistimer.so ../torch/lib/tmp_install/lib/
+cp roeistimer.h ..
---
+++ timing/roeistimer.h
@@ -0,0 +1,55 @@
+#ifndef __TIMER_H__
+#define __TIMER_H__
+
+extern "C" const char* roeis_func_names[];
+extern "C" uint64_t roeis_times[];
+extern "C" uint32_t roeis_funcs_len;
+
+//inline void roeis_collect_time(const char* func_name);
+//inline void roeis_report_time();
+//inline void roeis_clear_time();
+static inline uint32_t rdtscp() {
+  uint32_t rv;
+  asm volatile ("rdtscp": "=a" (rv) :: "edx", "ecx");
+  return rv;
+}
+
+static inline uint64_t rdtscp64() {
+  uint32_t low, high;
+  asm volatile ("rdtscp": "=a" (low), "=d" (high) :: "ecx");
+  return (((uint64_t)high) << 32) | low;
+}
+
+inline void roeis_collect_time(const char* func_name) {
+	//commented-out to save some cycles
+	//if (roeis_funcs_len > MAX_FUNCS) {
+	//	return;
+	//}
+        //printf("collect %d\n",roeis_funcs_len);
+	roeis_times[roeis_funcs_len] = rdtscp64();
+        //printf("collect2 %d\n",roeis_funcs_len);
+	roeis_func_names[roeis_funcs_len] = func_name;
+	roeis_funcs_len++;
+}
+
+inline void roeis_report_time() {
+	if (roeis_funcs_len == 0) {
+		return;
+	}
+	uint64_t prev_time = roeis_times[0];
+	for (int i = 1; i < roeis_funcs_len; i++) {
+                int64_t diff = roeis_times[i] - prev_time;
+                if (diff < 0) {//underflow
+			diff = prev_time - roeis_times[i];
+		}
+		printf("%s --> %s %lu\n",roeis_func_names[i-1], roeis_func_names[i], diff);
+		prev_time = roeis_times[i];
+	}
+}
+
+inline void roeis_clear_time() {
+        //printf("clean %d\n",roeis_funcs_len);
+	roeis_funcs_len = 0;
+}
+
+#endif
