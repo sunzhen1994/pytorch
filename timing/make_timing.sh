2d94ac17992d4845f5bcc7c0e703a0c01d367a77
Zhen Sun <zs352@cornell.edu>
2019-05-16 19:47:36 -0400

    changed paths

--- timing/make_timing.sh
+++ timing/make_timing.sh
@@ -2,7 +2,7 @@
 
 gcc -c -Wall -Werror -fpic roeistimer.c &&
 gcc -shared -o libroeistimer.so roeistimer.o &&
-cp libroeistimer.so /home/roei/.local/lib/python2.7/site-packages/torch/lib/
+cp libroeistimer.so /home/zhen/.local/lib/python2.7/site-packages/torch/lib/
 #cp libroeistimer.so /usr/local/lib/python2.7/dist-packages/torch/lib/
 cp libroeistimer.so ../build/lib/
 cp libroeistimer.so ../build/lib.linux-x86_64-2.7/torch/lib/
